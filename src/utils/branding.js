0b78353a3220f02f4b40ae591909c438fd60d67e
Alexg536 <alexandergrundel307@gmail.com>
2026-01-29 18:10:19 +0000

    feat: global commands + latest changes

--- src/utils/branding.js
+++ src/utils/branding.js
@@ -1,5 +1,5 @@
 const MODULO_NAME = "MODULO";
-const MODULO_ICON = ""; // optional: direct image URL (.png/.jpg/.webp), best: Discord CDN
+const MODULO_ICON = "https://media.discordapp.net/attachments/1457517538601603307/1457811095606661140/MODULO-logo-slogen-klein_1.png?ex=697862a2&is=69771122&hm=9f2763a581d03fb69548f3c8f70a56dccaacdb737d1bcf0221b0f19888d291bc&=&format=webp&quality=lossless&width=685&height=856"; // optional: direct image URL (.png/.jpg/.webp), best: Discord CDN
 
 function isValidImageUrl(url) {
   if (!url) return false;
